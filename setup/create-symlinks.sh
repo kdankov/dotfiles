7d9c4e883f32674c2f69d7e240f8520eefc54634
Konstantin Dankov <konstantin.dankov@gmail.com>
2015-09-16 15:31:18 +0300

    Updates all around

--- setup/create-symlinks.sh
+++ setup/create-symlinks.sh
@@ -5,33 +5,31 @@ echo -e "----------------------------------------------------"
 echo -e "--- Setting up all Symlinks ------------------------"
 echo -e "---------------------------------------------------- \n"
 
-echo -e "--- Bash --- \n"
+echo -e "--- Bash --- "
 ln -s ~/.dotfiles/config/bashrc ~/.bash_profile
-echo -e "--- DONE --- ln -s ~/.dotfiles/config/bashrc ~/.bash_profile \n \n"
+echo -e "| --- DONE --- \n \n"
 
-echo -e "--- Git --- \n"
+echo -e "--- Git --- "
 ln -s ~/.dotfiles/config/git/gitconfig ~/.gitconfig
-echo -e "--- DONE --- ln -s ~/.dotfiles/config/git/gitconfig ~/.gitconfig \n \n"
+echo -e "| --- DONE --- \n \n"
 
-echo -e "--- Git --- \n"
+echo -e "--- Git --- "
 ln -s ~/.dotfiles/config/ack/ackrc ~/.ackrc
-echo -e "--- DONE --- ln -s ~/.dotfiles/config/git/gitconfig ~/.gitconfig \n \n"
+echo -e "| --- DONE --- \n \n"
 
-echo -e "--- Vim --- \n"
+echo -e "--- Vim --- "
 ln -s ~/.dotfiles/config/vim ~/.vim
 ln -s ~/.dotfiles/config/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/config/vim/gvimrc ~/.gvimrc
-echo -e "--- DONE --- ln -s ~/.dotfiles/config/vim/vimrc ~/.vimrc \n \n"
+echo -e "| --- DONE --- \n \n"
 
-echo -e "--- Consular --- \n"
-ln -s ~/.dotfiles/config/consularc ~/.consularc
-
-if [ ! -d "~/.config" ]; then
-	mkdir ~/.config
-	ln -s ~/.dotfiles/config/consular ~/.config/consular
-fi
-
-echo -e "--- DONE --- ln -s ~/.dotfiles/config/consularc ~/.consularc \n \n"
+#echo -e "--- Consular --- \n"
+#ln -s ~/.dotfiles/config/consularc ~/.consularc
+#if [ ! -d "~/.config" ]; then
+	#mkdir ~/.config
+	#ln -s ~/.dotfiles/config/consular ~/.config/consular
+#fi
+#echo -e "| --- DONE --- \n \n"
 
 echo -e "----------------------------------------------------"
 echo -e "--- Symlinks set -----------------------------------"
