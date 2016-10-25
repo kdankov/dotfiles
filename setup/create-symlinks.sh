1dc1ebde77a4fba2ce0f9ba3e07579d00aa26145
Konstantin Dankov <konstantin.dankov@gmail.com>
2016-10-25 15:48:01 +0300

    Update

--- setup/create-symlinks.sh
+++ setup/create-symlinks.sh
@@ -17,20 +17,6 @@ echo -e "--- Git --- "
 ln -s ~/.dotfiles/config/ack/ackrc ~/.ackrc
 echo -e "| --- DONE --- \n \n"
 
-echo -e "--- Vim --- "
-ln -s ~/.dotfiles/config/vim ~/.vim
-ln -s ~/.dotfiles/config/vim/vimrc ~/.vimrc
-ln -s ~/.dotfiles/config/vim/gvimrc ~/.gvimrc
-echo -e "| --- DONE --- \n \n"
-
-#echo -e "--- Consular --- \n"
-#ln -s ~/.dotfiles/config/consularc ~/.consularc
-#if [ ! -d "~/.config" ]; then
-	#mkdir ~/.config
-	#ln -s ~/.dotfiles/config/consular ~/.config/consular
-#fi
-#echo -e "| --- DONE --- \n \n"
-
 echo -e "----------------------------------------------------"
 echo -e "--- Symlinks set -----------------------------------"
 echo -e "---------------------------------------------------- \n"
