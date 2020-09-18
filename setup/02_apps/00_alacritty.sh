cc1b14a1fbba5004cafaecb1a8cadbcb0feb93cf
Konstantin Dankov <konstantin.dankov@gmail.com>
2020-09-18 13:21:16 +0300

    Add alacritty setup

---
+++ setup/02_apps/00_alacritty.sh
@@ -0,0 +1,9 @@
+cd ~
+
+echo -e "----------------------------------------------------"
+echo -e "--- Setting up Alacrity - - - - -------------------------"
+echo -e "---------------------------------------------------- \n"
+
+ln -s ~/.dotfiles/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+echo -e "--- Setting up Alacritty - - - DONE ---------------------- \n \n"
