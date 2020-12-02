02b9c3443b7ba9f9b3ffbfa1d4e7a2c4e49ce2ce
Konstantin Dankov <konstantin.dankov@givingforce.com>
2020-12-02 10:31:32 +0200

    Progress

--- setup/02_apps/00_alacritty.sh
+++ setup/02_apps/00_alacritty.sh
@@ -1,9 +1,9 @@
 cd ~
 
-echo -e "----------------------------------------------------"
-echo -e "--- Setting up Alacrity - - - - -------------------------"
-echo -e "---------------------------------------------------- \n"
+echo "----------------------------------------------------"
+echo "--- Setting up Alacrity - - - - -------------------------"
+echo "---------------------------------------------------- \n"
 
 ln -s ~/.dotfiles/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-echo -e "--- Setting up Alacritty - - - DONE ---------------------- \n \n"
+echo "\n--- Setting up Alacritty - - - DONE ---------------------- \n \n"
--- setup/02_apps/02_vim.sh
+++
@@ -1,9 +0,0 @@
-cd ~
-
-echo -e "----------------------------------------------------"
-echo -e "--- Setting up vim - - - - -------------------------"
-echo -e "---------------------------------------------------- \n"
-
-git clone https://github.com/kdankov/vim.git .vim
-
-echo -e "--- Setting up vim - - - DONE ---------------------- \n \n"
