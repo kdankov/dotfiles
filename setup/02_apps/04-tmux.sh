e0095d007c63f0076e94b94864820f13f46acd7b
Konstantin Dankov <konstantin.dankov@gmail.com>
2021-11-07 23:50:49 +0200

    Tweaks and Updates

--- setup/02_apps/04-tmux.sh
+++ setup/02_apps/04-tmux.sh
@@ -4,6 +4,9 @@ echo "----------------------------------------------------"
 echo "--- Setting up tmux  - - - -------------------------"
 echo "---------------------------------------------------- \n"
 
+mkdir ~/.config/tmux
+mkdir ~/.config/tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 ln -s ~/.dotfiles/config/tmux/tmux.conf ~/.tmux.conf
 
 echo "\n--- Setting up tmux - - - DONE --------------------- \n"
