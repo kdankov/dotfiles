6f2081f1e1333f0a3d2941d247569a093c83fce8
Konstantin Dankov <konstantin.dankov@gmail.com>
2023-08-29 14:40:15 +0300

    ZSH Config update

--- setup/01_homebrew/02_apps.sh
+++ setup/01_homebrew/02_apps.sh
@@ -2,7 +2,7 @@
 apps=(
   1password
   dropbox
-  iterm2
+#  iterm2
   firefox
   chromium
   alfred
@@ -10,23 +10,18 @@ apps=(
   telegram
   slack
   skype
-  4k-video-downloader
-  4k-youtube-to-mp3
   adobe-creative-cloud
-  transmission
-  gitup
   istat-menus
   sequel-pro
   viber
   spotify
-  virtualbox
   moom
 )
 
 # Install apps to /Applications
 # Default is: /Users/$user/Applications
 echo "Installing Apps ..."
-brew cask install --appdir="/Applications" ${apps[@]}
+brew install --cask ${apps[@]}
 
 # Cleanup
 brew cleanup
